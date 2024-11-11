3e99dd4821c7f9a9ba1fa075c63f24d0a2b3e8f8
Bill Xi <86190295+TurkeyBilly@users.noreply.github.com>
2024-11-11 18:55:53 -0500

    fix constructor parameters

--- src/main/java/app/SubEventCreator.java
+++ src/main/java/app/SubEventCreator.java
@@ -2,8 +2,6 @@ package app;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
-import discord4j.core.event.domain.lifecycle.ReadyEvent;
-import discord4j.core.object.entity.User;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
 
@@ -13,18 +11,24 @@ public class SubEventCreator<E extends discord4j.core.event.domain.Event> {
     DiscordClient client;
     Mono<Void> event;
     GatewayDiscordClient gateway;
-    Function<E, Mono<Void>> eventMapper;
+    Function<E, Publisher<Void>> eventMapper;
 
     public SubEventCreator(DiscordClient client, GatewayDiscordClient gateway, Mono<Void> event){
-
+        this.client = client;
+        this.event = event;
         this.gateway = gateway;
     }
 
-    public SubEventCreator(DiscordClient client, GatewayDiscordClient gateway, Function<E, Mono<Void>> eventMapper) {
-
+    public SubEventCreator(DiscordClient client, GatewayDiscordClient gateway, Class<E> eventClass,
+                           Function<E, Publisher<Void>> eventMapper) {
+        this.client = client;
+        this.gateway = gateway;
+        this.eventMapper = eventMapper;
+        this.event = mapperToEvent(gateway, eventClass, eventMapper);
     }
 
-    public Mono<Void> mapperToEvent(GatewayDiscordClient gateway, Class<E> eventClass, Function<E, Publisher<Void>> eventMapper){
+    public Mono<Void> mapperToEvent(GatewayDiscordClient gateway, Class<E> eventClass,
+                                    Function<E, Publisher<Void>> eventMapper){
         return gateway.on(eventClass, eventMapper).then();
     }
 
