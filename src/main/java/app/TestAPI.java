a57267ba878816f43c4287acdf16792df104d42b
Julia Zhong Guo <juliagzl1215@outlook.com>
2024-11-11 14:30:55 -0500

    updated maven configuration and tested API

---
+++ src/main/java/app/TestAPI.java
@@ -0,0 +1,28 @@
+package app;
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import reactor.core.publisher.Mono;
+
+public class TestAPI {
+    public static void main(String[] args) {
+        DiscordClient client = DiscordClient.create("MTEzODg1MzczMzQ2MDEwMzMzOQ.G9wYsT.OpiQGsUnUJ4KoIKmSD0L9CDCETNzQhZPBCDFto");
+
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+                gateway.on(MessageCreateEvent.class, event -> {
+                    Message message = event.getMessage();
+
+                    if (message.getContent().equalsIgnoreCase("!ping")) {
+                        return message.getChannel()
+                                .flatMap(channel -> channel.createMessage("pong!"));
+                    }
+
+                    return Mono.empty();
+                }));
+
+        login.block();
+    }
+}
