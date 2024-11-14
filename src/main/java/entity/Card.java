0373ef7ea3b355968dabf95caf370030def6eefa
li-tianchu <tianchu.li@mail.utoronto.ca>
2024-11-14 17:12:51 -0500

    basic entity logics

--- src/main/java/entity/Card.java
+++ src/main/java/entity/Card.java
@@ -1,4 +1,31 @@
 package entity;
 
+/**
+ * Entity that represents a card.
+ *  - id: ID of the card.
+ *  - name: Name of the card to be displayed on the GUI.
+ *  - iconPath: Path to the icon file to be displayed on the GUI.
+ */
 public class Card {
+    private int id;
+    private String name;
+    private String iconPath;
+
+    public Card(int id, String displayName, String iconPath) {
+        this.id = id;
+        this.name = displayName;
+        this.iconPath = iconPath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIconPath() {
+        return iconPath;
+    }
 }
--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -1,4 +1,48 @@
 package entity;
 
+import java.util.List;
+
+/**
+ * Class that represents a player.
+ *  - id: ID of the player.
+ *  - score: Score obtained by the player.
+ *  - name: Display name of the player.
+ *  - hand: List of Cards that the player is holding.
+ */
 public class Player {
+    private int id;
+    private int score;
+    private String name;
+    private List<Card> hand;
+
+    public Player(int id, int score, String name, List<Card> hand) {
+        this.id = id;
+        this.score = score;
+        this.name = name;
+        this.hand = hand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setHand(List<Card> hand) {
+        this.hand = hand;
+    }
 }
