506f189eb100a5869019f8524278a1f48bb66d96
Bill Xi <86190295+TurkeyBilly@users.noreply.github.com>
2024-11-13 19:38:13 -0500

    Refractor message detack -> sending logic into clean methods (1/3)

--- src/main/java/app/StatusEventCreator.java
+++ src/main/java/app/StatusEventCreator.java
@@ -3,27 +3,31 @@ package app;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Member;
-import discord4j.core.object.entity.Message;
 import reactor.core.publisher.Mono;
 
-import java.util.Optional;
 
 public class StatusEventCreator extends SubEventCreator<MessageCreateEvent> {
+
     public StatusEventCreator(DiscordClient client, GatewayDiscordClient gateway) {
         super(client, gateway, MessageCreateEvent.class, event -> {
-            Message message = event.getMessage();
-            Optional<Member> member = event.getMember();
+            String memberName = SubEventCreator.getMemberName(event);
+            String content = SubEventCreator.getContent(event);
 
-            String memberName = member.map(Member::getUsername).orElse("Guest");
-            String content = message.getContent();
-
-            if (content.equalsIgnoreCase("report status")) {
-                return message.getChannel()
-                        .flatMap(channel -> channel.createMessage("Dear " + memberName + " , everything is fine").then());
+            if (invokeMessage(content)) {
+                return sendMessage(event, newMessage(memberName));
             }
 
             return Mono.empty();
         });
     }
+
+    public static String newMessage(String memberName) {
+        return "Dear " + memberName + " , everything is fine";
+    }
+
+    public static boolean invokeMessage(String message) {
+        return message.equalsIgnoreCase("report status");
+    }
+
+
 }
--- src/main/java/app/SubEventCreator.java
+++ src/main/java/app/SubEventCreator.java
@@ -2,12 +2,16 @@ package app;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Message;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
+import discord4j.core.event.domain.Event;
 
 import java.util.function.Function;
 
-public class SubEventCreator<E extends discord4j.core.event.domain.Event> {
+public class SubEventCreator<E extends Event> {
     DiscordClient client;
     Mono<Void> event;
     GatewayDiscordClient gateway;
@@ -34,5 +38,19 @@ public class SubEventCreator<E extends discord4j.core.event.domain.Event> {
 
     public Mono<Void> getEvent() {
         return event;
+    }
+
+    public static String getContent(MessageCreateEvent fromEvent) {
+        return fromEvent.getMessage().getContent();
+    }
+
+    public static String getMemberName(MessageCreateEvent fromEvent) {
+        return fromEvent.getMember().map(Member::getUsername).orElse("Guest");
+    }
+
+    public static Mono<Void> sendMessage(MessageCreateEvent fromEvent, String text) {
+        Message message = fromEvent.getMessage();
+        return message.getChannel()
+                .flatMap(channel -> channel.createMessage(text).then());
     }
 }
