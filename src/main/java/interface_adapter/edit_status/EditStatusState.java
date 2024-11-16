d43c89d2c49d2b8cd8027f0d90ef48ac51c74c7f
Julia Zhong Guo <juliagzl1215@outlook.com>
2024-11-15 22:00:54 -0500

    feat(interface_adapter.editStatus): EditStatusState & SelectDoraState

--- src/main/java/interface_adapter/edit_status/EditStatusState.java
+++ src/main/java/interface_adapter/edit_status/EditStatusState.java
@@ -1,17 +1,56 @@
 package interface_adapter.edit_status;
 
+import mahjong.BaseTiles;
+
+/**
+ * The state information representing the statuses being edited.
+ */
 public class EditStatusState {
 
-        private String status;
+    private Boolean[] editStatus = new Boolean[5];
+    private Boolean[] redDoraStatus = new Boolean[3];
+    private BaseTiles[] doraIndicators = new BaseTiles[5];
+    private String selectIndicatorError;
 
-        public EditStatusState() {
-        }
+    // Setters and getters
+    public Boolean[] getEditStatus() {
+        return editStatus;
+    }
 
-        public String getStatus() {
-            return status;
-        }
+    public Boolean[] getRedDoraStatus() {
+        return redDoraStatus;
+    }
 
-        public void setStatus(String status) {
-            this.status = status;
-        }
+    public BaseTiles[] getDoraIndicators() {
+        return doraIndicators;
+    }
+
+    public String getSelectIndicatorError() {
+        return selectIndicatorError;
+    }
+
+    public void setEditStatus(Boolean[] editStatus) {
+        this.editStatus = editStatus;
+    }
+
+    public void setRedDoraStatus(Boolean[] redDoraStatus) {
+        this.redDoraStatus = redDoraStatus;
+    }
+
+    public void setDoraIndicators(BaseTiles[] doraIndicators) {
+        this.doraIndicators = doraIndicators;
+    }
+
+    public void setSelectIndicatorError(String selectIndicatorError) {
+        this.selectIndicatorError = selectIndicatorError;
+    }
+
+    @Override
+    public String toString() {
+        return "EditStatusState{" +
+                "editStatus=" + editStatus +
+                ", redDoraStatus=" + redDoraStatus +
+                ", doraIndicators=" + doraIndicators +
+                '}';
+    }
 }
--- src/main/java/interface_adapter/edit_status/SelectDoraState.java
+++ src/main/java/interface_adapter/edit_status/SelectDoraState.java
@@ -1,4 +1,39 @@
 package interface_adapter.edit_status;
 
+import mahjong.BaseTiles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The state information representing the dora indicator selections.
+ */
 public class SelectDoraState {
+    private Boolean[][] doraSelections = new Boolean[4][9];
+
+    // Getters and setters
+    public Boolean[][] getDoraSelections() {
+        return doraSelections;
+    }
+
+    public void setDoraSelections(Boolean[][] doraSelections) {
+        this.doraSelections = doraSelections;
+    }
+
+    @Override
+    public String toString() {
+        List<BaseTiles> selectedTiles = new ArrayList<>();
+
+        for (int i = 0; i < doraSelections.length; i++) {
+            for (int j = 0; j < doraSelections[i].length; j++) {
+                if (doraSelections[i][j]) {
+                    selectedTiles.add(BaseTiles.values()[i * 9 + j]);
+                }
+            }
+        }
+
+        return "SelectDoraState{" +
+                "doraSelections=" + selectedTiles +
+                '}';
+    }
 }
