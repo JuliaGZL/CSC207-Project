4afc827efe8b9609e1ea50f080995557a7bfb674
aiyizhou <yizhou.ai@mail.utoronto.ca>
2024-11-18 00:18:15 -0400

    feat: add sortBody method and getCompletedTiles method for processing tile sets

--- src/mahjong/CompletedTiles.java
+++ src/mahjong/CompletedTiles.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Represents a completed set of tiles in Mahjong, consisting of a head and body groups.
+ * Represents a completed set of tiles in Mahjong, consisting of a head and body
+ * groups.
  */
 class CompletedTiles implements Comparable<CompletedTiles> {
 
@@ -60,6 +61,57 @@ class CompletedTiles implements Comparable<CompletedTiles> {
    */
   public List<TileGroup> getBody() {
     return body;
+  }
+
+  /**
+   * Sorts the tiles within each TileGroup in the body and then sorts the body
+   * itself.
+   * This method first calls the sortTiles method on each TileGroup in the body to
+   * sort the tiles within each group.
+   * After that, it sorts the body list itself.
+   */
+  public void sortBody() {
+    for (TileGroup g : body) {
+      g.sortTiles();
+    }
+    body.sort(null);
+  }
+
+  /**
+   * This method processes a list of BaseTile objects to find all possible
+   * completed tile sets.
+   * A completed tile set is a valid Mahjong hand that can be formed from the
+   * given tiles.
+   * 
+   * @param tiles the list of BaseTile objects to be processed. The list must
+   *              contain a number of tiles
+   *              that is congruent to 2 modulo 3 (i.e., the size of the list must
+   *              be of the form 3n + 2).
+   * @return a list of unique CompletedTiles objects representing all possible
+   *         completed tile sets.
+   * @throws RuntimeException if the number of tiles is not congruent to 2 modulo
+   *                          3.
+   */
+  public static List<CompletedTiles> getCompletedTiles(List<BaseTile> tiles) {
+    if (tiles.size() % 3 != 2) {
+      throw new RuntimeException("Not Enough Tiles");
+    }
+    tiles.sort(null);
+    TileSplitter inst = TileSplitter.getInstance();
+    inst.reset();
+    List<CompletedTiles> completedTiles = inst.getAllCompletedTiles(tiles);
+    for (CompletedTiles completed : completedTiles) {
+      completed.sortBody();
+    }
+    completedTiles.sort(null);
+    List<CompletedTiles> uniqueCompletedTiles = new ArrayList<>();
+    for (CompletedTiles completed : completedTiles) {
+      if (!uniqueCompletedTiles.contains(completed)) {
+        uniqueCompletedTiles.add(completed);
+      }
+    }
+    completedTiles = uniqueCompletedTiles;
+    return completedTiles;
   }
 
   @Override
