990d031f8d0cc833b99b20cc24c5024677db7478
aiyizhou <yizhou.ai@mail.utoronto.ca>
2024-11-22 14:58:57 -0400

    refacotr: remove redundancy

---
+++ src/main/java/use_case/remove_tile/edit_status/EditStatusInputBoundary.java
@@ -0,0 +1,18 @@
+package usecase.edit_status;
+
+/**
+ * The Edit Status Use Case.
+ */
+public interface EditStatusInputBoundary {
+
+    /**
+     * Execute the Edit Status Use Case.
+     * @param editStatusInputData the input data for this use case
+     */
+    void execute(EditStatusInputData editStatusInputData);
+
+    /**
+     * Executes the switch to select dora use case.
+     */
+    void switchToSelectDoraView();
+}
---
+++ src/main/java/use_case/remove_tile/edit_status/SelectDoraInteractor.java
@@ -0,0 +1,8 @@
+package usecase.edit_status;
+
+/**
+ * The Select Dora Interactor.
+ */
+public class SelectDoraInteractor {
+
+}
