930d78b6100cfe29b3d28019251bc465f9ed07f6
Bill Xi <86190295+TurkeyBilly@users.noreply.github.com>
2024-11-22 18:18:09 -0500

    add leave command into test

---
+++ src/main/java/usecase/api_usecase/DefaultBotFactory.java
@@ -0,0 +1,30 @@
+package usecase.api_usecase;
+
+import discord4j.core.event.domain.message.MessageEvent;
+
+public class DefaultBotFactory {
+    String token;
+    ChatBot bot;
+
+    public DefaultBotFactory() {
+        String token = "MTEzODg1MzczMzQ2MDEwMzMzOQ.G9wYsT.OpiQGsUnUJ4KoIKmSD0L9CDCETNzQhZPBCDFto";
+        this.token = token;
+        bot = new ChatBot(token);
+        bot.addEvent(LoginEvent.class);
+        bot.addEvent(GreetingEvent.class);
+        bot.addEvent(MahjongComboInteractor.class);
+    }
+
+    public DefaultBotFactory(String token) {
+        this.token = token;
+        bot = new ChatBot(token);
+    }
+
+    public ChatBot getBot() {
+        return bot;
+    }
+
+    public void run() {
+        bot.run();
+    }
+}
--- src/main/java/usecase/api_usecase/TestFinalChatBot.java
+++ src/main/java/usecase/api_usecase/TestFinalChatBot.java
@@ -2,11 +2,16 @@ package usecase.api_usecase;
 
 public class TestFinalChatBot {
     public static void main(String[] args) {
-        String token = "MTEzODg1MzczMzQ2MDEwMzMzOQ.G9wYsT.OpiQGsUnUJ4KoIKmSD0L9CDCETNzQhZPBCDFto";
+        // String token = "MTEzODg1MzczMzQ2MDEwMzMzOQ.G9wYsT.OpiQGsUnUJ4KoIKmSD0L9CDCETNzQhZPBCDFto";
 
-        ChatBot bot = new ChatBot(token);
-        bot.addEvent(LoginEvent.class);
-        bot.addEvent(GreetingEvent.class);
-        bot.run();
+        // ChatBot bot = new ChatBot(token);
+        // bot.addEvent(LoginEvent.class);
+        // bot.addEvent(GreetingEvent.class);
+        // bot.run();
+
+        // System.out.println("First round ended");
+
+        DefaultBotFactory factory = new DefaultBotFactory();
+        factory.getBot().run();
     }
 }
