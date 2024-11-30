f5a7b7323e3bcb215ac8f994ec65d396f0669bb3
Moebie Wu <mcpro233@gmail.com>
2024-11-30 14:37:10 -0500

    refactor: moved tests for chatbot api and score computer into appropriate packages

--- src/test/java/integrated/chatbot_api/TestAPI.java
+++ src/test/java/integrated/chatbot_api/TestAPI.java
@@ -1,10 +1,8 @@
-package app;
+package integrated.chatbot_api;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
-import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.User;
 import reactor.core.publisher.Mono;
 
 public class TestAPI {
