a9e5ccb0d304b002b1be23fcf8f3dc432ad4056d
Bill Xi <86190295+TurkeyBilly@users.noreply.github.com>
2024-12-01 00:08:34 -0500

    reorganize greeting handler

--- src/main/java/data_access/discord_bot/GreetingHandler.java
+++ src/main/java/data_access/discord_bot/GreetingHandler.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GreetingHandler extends MessageHandler {
-    static final List<String> GREETING_LIST = Arrays.asList("hello", "hi", "hey", "你好");
+    static final List<String> GREETING_LIST = Arrays.asList("hello", "hi", "hey", "你好", "こんにちは");
 
     public GreetingHandler(DiscordClient client, GatewayDiscordClient gateway) {
         super(client, gateway, MessageCreateEvent.class, event -> {
