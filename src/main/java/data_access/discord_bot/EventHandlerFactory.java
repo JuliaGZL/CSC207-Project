43b5d59c6f95adf92bc7cf454d333e41b4dc6148
Bill Xi <86190295+TurkeyBilly@users.noreply.github.com>
2024-12-02 21:12:27 -0500

    Style check event handler factory

--- src/main/java/data_access/discord_bot/EventHandlerFactory.java
+++ src/main/java/data_access/discord_bot/EventHandlerFactory.java
@@ -3,7 +3,24 @@ package data_access.discord_bot;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 
+/**
+ * Interface for creating event handlers for Discord bot events.
+ */
 public interface EventHandlerFactory {
-    public EventHandler createEventHandler(Class<? extends EventHandler> eventType,
-                                           DiscordClient client, GatewayDiscordClient gateway);
+
+    /**
+     * Creates an event handler for the specified event type.
+     *
+     * @param eventType the class type of the
+     *                  event handler to create
+     * @param client the Discord client used
+     *               to connect to Discord
+     * @param gateway the GatewayDiscordClient
+     *                used to interact with the Discord gateway
+     * @return the created event handler
+     */
+    EventHandler createEventHandler(
+            Class<? extends EventHandler> eventType,
+            DiscordClient client,
+            GatewayDiscordClient gateway);
 }
