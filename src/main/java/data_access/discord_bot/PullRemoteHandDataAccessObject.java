1fccbdcb5202b6a2cbbcf4e03a839fdfacc647ba
Bill Xi <86190295+TurkeyBilly@users.noreply.github.com>
2024-12-02 22:23:00 -0500

    Refractor DataAccessObject

--- src/main/java/data_access/discord_bot/PullRemoteHandDataAccessObject.java
+++ src/main/java/data_access/discord_bot/PullRemoteHandDataAccessObject.java
@@ -10,13 +10,31 @@ import java.util.List;
 /**
  * DAO for the pull hand from discord use case.
  */
-public class PullRemoteHandDataAccessObject implements PullRemoteHandDataAccessInterface {
+public class PullRemoteHandDataAccessObject implements
+        PullRemoteHandDataAccessInterface {
+
+    /**
+     * In-memory data access object used for player data management.
+     */
     private final InMemoryUniversalDataAccessObject universalDAO;
 
-    public PullRemoteHandDataAccessObject(InMemoryUniversalDataAccessObject universalDAO) {
+    /**
+     * Constructor for PullRemoteHandDataAccessObject.
+     *
+     * @param universalDAO the in-memory data access object used for player
+     *                     data operations
+     */
+    public PullRemoteHandDataAccessObject(
+            final InMemoryUniversalDataAccessObject universalDAO) {
         this.universalDAO = universalDAO;
     }
 
+    /**
+     * Retrieves the hand from Discord by activating the bot and obtaining
+     * the message from it.
+     *
+     * @return a list of tiles based on the Discord bot message
+     */
     @Override
     public List<Tile> getHandFromDiscord() {
         DefaultBotBuilder chatbotBuilder = new DefaultBotBuilder();
@@ -31,7 +49,7 @@ public class PullRemoteHandDataAccessObject implements PullRemoteHandDataAccessI
      * @return true if a player with the given name exists; false otherwise
      */
     @Override
-    public boolean existsByName(String playerName) {
+    public boolean existsByName(final String playerName) {
         return universalDAO.existsByName(playerName);
     }
 
@@ -41,7 +59,7 @@ public class PullRemoteHandDataAccessObject implements PullRemoteHandDataAccessI
      * @param player the player to save
      */
     @Override
-    public void savePlayer(Player player) {
+    public void savePlayer(final Player player) {
         universalDAO.savePlayer(player);
     }
 
@@ -52,7 +70,7 @@ public class PullRemoteHandDataAccessObject implements PullRemoteHandDataAccessI
      * @return the user with the given username
      */
     @Override
-    public Player getPlayer(String playerName) {
+    public Player getPlayer(final String playerName) {
         return universalDAO.getPlayer(playerName);
     }
 }
